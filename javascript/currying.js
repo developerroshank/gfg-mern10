fce6cc13d1fdd71405d1c11aa8670798e5df1d50
developerroshank <developerroshank@gmail.com>
2025-09-16 20:57:31 +0530

    currying functional programming approach

---
+++ javascript/currying.js
@@ -0,0 +1,19 @@
+// function autoMail(to, subject, body) {
+//   console.log(`Sending mail to: ${to} with subject: ${subject} and body: ${body}`);
+// }
+
+// autoMail("abc@gmail.com", "Test Mail", "thank you for shopping with us");
+
+
+// functional programming approch --- currying
+
+function autoMailer(to) {
+  return function (subject) {
+    return function (body) {
+      console.log(`Sending mail to: ${to} with subject: ${subject} and body: ${body}`);
+    }
+  }
+}
+
+autoMailer("abc@gmail.com")("Test Mail")("thank you for shopping with us");
+
