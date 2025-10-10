963cd03dddfdede43c9a43f45e1a0d6f8486585b
developerroshank <developerroshank@gmail.com>
2025-10-10 22:06:23 +0530

    usememo

--- react/usememo/src/App.jsx
+++ react/usememo/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  const sum = () => {
+    console.log('I am a heavy task dont run me')
+    let sum = 0;
+    for (let i = 0; i < 10000000000; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
+
+  // const res = sum();
+  const res = useMemo(sum, []);
+
   return (
     <div>
-      <button onClick={() => setCount(count+1)}>Click {count}</button>
+      <p>Result of heavy computation task: {res}</p>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
   )
 }
